2369ccd25ffcedd398062a3188e74b075a0a5ea0
xx01cyx <caoyuanxin0531@outlook.com>
2021-07-15 14:39:50 +0800

    fix: convert UserCard from StatefulWidget to StatelessWidget

--- lib/widgets/user_card.dart
+++ lib/widgets/user_card.dart
@@ -5,123 +5,122 @@ import 'package:zhihu_demo/assets/custom_styles.dart';
 import 'package:zhihu_demo/model/user_info.dart';
 import 'package:zhihu_demo/widgets/follow_button.dart';
 
-class UserCard extends StatefulWidget {
+class UserCard extends StatelessWidget {
 
   final UserCardInfo _userCardInfo;
 
-  UserCard(this._userCardInfo, {Key? key}) : super(key: key);
-
-  @override
-  _UserCardState createState() => _UserCardState();
-}
-
-class _UserCardState extends State<UserCard> {
+  const UserCard(this._userCardInfo, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: Constants.defaultUserCardHeight,
       child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-          ),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.all(Constants.defaultCardPadding),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Expanded(
-                  flex: 15,
-                  child: Container(
-                    width: 55.0,
-                    height: 55.0,
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+        ),
+        child: Container(
+          width: MediaQuery
+              .of(context)
+              .size
+              .width,
+          padding: EdgeInsets.all(Constants.defaultCardPadding),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 15,
+                child: Container(
+                  width: 55.0,
+                  height: 55.0,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
                         "http://img8.zol.com.cn/bbs/upload/23765/23764201.jpg"),
+                  ),
+                ),
+              ),
+
+              Expanded(
+                flex: 3,
+                child: Container(),
+              ),
+
+              Expanded(
+                flex: 51,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 36,
+                      child: Text(
+                        _userCardInfo.userName,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: CustomStyles.userNameStyle,
+                      ),
                     ),
-                  ),
-                ),
-
-                Expanded(
-                  flex: 3,
-                  child: Container(),
-                ),
-
-                Expanded(
-                  flex: 51,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Expanded(
-                        flex: 36,
-                        child: Text(
-                          widget._userCardInfo.userName,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          style: CustomStyles.userNameStyle,
+                    Expanded(
+                      flex: 3,
+                      child: Container(),
+                    ),
+                    Expanded(
+                      flex: 28,
+                      child: Text(
+                        _userCardInfo.brief,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: CustomStyles.userBriefStyle,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Container(),
+                    ),
+                    Expanded(
+                      flex: 28,
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            WidgetSpan(
+                              child: CustomStyles.getDefaultReplyIcon(),
+                            ),
+                            TextSpan(
+                              text: ' ${_userCardInfo.commentCount}   ',
+                            ),
+                            WidgetSpan(
+                              child: CustomStyles
+                                  .getDefaultFilledFollowerIcon(),
+                            ),
+                            TextSpan(
+                              text: ' ${_userCardInfo.followerCount} ',
+                            ),
+                          ],
+                          style: CustomStyles.postFooterStyle,
                         ),
                       ),
-                      Expanded(
-                        flex: 3,
-                        child: Container(),
-                      ),
-                      Expanded(
-                        flex: 28,
-                        child: Text(
-                          widget._userCardInfo.brief,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          style: CustomStyles.userBriefStyle,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 5,
-                        child: Container(),
-                      ),
-                      Expanded(
-                        flex: 28,
-                        child: RichText(
-                          text: TextSpan(
-                            children: [
-                              WidgetSpan(
-                                child: CustomStyles.getDefaultReplyIcon(),
-                              ),
-                              TextSpan(
-                                text: ' ${widget._userCardInfo.commentCount}   ',
-                              ),
-                              WidgetSpan(
-                                child: CustomStyles.getDefaultFollowerIcon(),
-                              ),
-                              TextSpan(
-                                text: ' ${widget._userCardInfo.followerCount} ',
-                              ),
-                            ],
-                            style: CustomStyles.postFooterStyle,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
+              ),
 
-                Expanded(
-                  flex: 2,
-                  child: Container(),
-                ),
+              Expanded(
+                flex: 2,
+                child: Container(),
+              ),
 
-                Expanded(
-                  flex: 29,
-                  child: FollowButton(
-                    widget._userCardInfo.userName,
-                    widget._userCardInfo.followStatus,
-                  ),
+              Expanded(
+                flex: 29,
+                child: FollowButton(
+                  _userCardInfo.userName,
+                  _userCardInfo.followStatus,
                 ),
-              ],
-            ),
-          )),
+              ),
+            ],
+          ),
+        ),),
     );
   }
 }
+
