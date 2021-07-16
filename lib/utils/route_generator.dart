0db3c87bde108bc7a0db78e957f1b233aea6b52e
Gun9niR <gun9nir.guo@gmail.com>
2021-07-16 09:42:17 +0800

    feat: login page

--- lib/utils/route_generator.dart
+++ lib/utils/route_generator.dart
@@ -1,3 +1,4 @@
+import 'package:comment_overflow/pages/login_page.dart';
 import 'package:comment_overflow/pages/new_post_page.dart';
 import 'package:comment_overflow/pages/search_page.dart';
 import 'package:comment_overflow/pages/search_result_page.dart';
@@ -10,11 +11,14 @@ class RouteGenerator {
   static const searchRoute = '/search';
   static const searchResultRoute = '/search_result';
   static const newPostRoute = '/new_post';
+  static const loginRoute = '/login';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments;
 
     switch (settings.name) {
+      case loginRoute:
+        return MaterialPageRoute(builder: (_) => LoginPage());
       case newPostRoute:
         return MaterialPageRoute(builder: (_) => NewPostPage());
       case searchRoute:
