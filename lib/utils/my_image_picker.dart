f2a8f080d7d1b10d44b48b92a8fd83496d959b7b
JolyneFr <jolynefr@sjtu.edu.cn>
2021-07-16 09:27:35 +0800

    feat: my image picker

---
+++ lib/utils/my_image_picker.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MyImagePicker {
+
+  List<PickedFile>? imageFileList;
+
+  set _imageFile(PickedFile? value) {
+    imageFileList = value == null ? null : [value];
+  }
+
+  dynamic pickImageError;
+  final ImagePicker _picker = ImagePicker();
+
+  void pickImage(ImageSource source,
+      {BuildContext? context, bool isMultiImage = false,
+        double? maxWidth, double? maxHeight, int? quality}) async {
+    if (isMultiImage) {
+      try {
+        final pickedFileList = await _picker.getMultiImage(
+          maxWidth: maxWidth,
+          maxHeight: maxHeight,
+          imageQuality: quality,
+        );
+        imageFileList = pickedFileList;
+      } catch (error) {
+        pickImageError = error;
+      }
+    } else {
+      try {
+        final pickedFile = await _picker.getImage(
+          source: source,
+          maxWidth: maxWidth,
+          maxHeight: maxHeight,
+          imageQuality: quality,
+        );
+        _imageFile = pickedFile;
+      } catch (error) {
+        pickImageError = error;
+      }
+    }
+  }
+
+}
