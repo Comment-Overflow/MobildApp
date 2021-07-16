4a40a757b9ce438c9c22d3468e558545f833b2eb
JolyneFr <jolynefr@sjtu.edu.cn>
2021-07-16 15:19:50 +0800

    feat: add vertical_image_scroller

---
+++ lib/widgets/vertical_image_scroller.dart
@@ -0,0 +1,84 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
+
+class VerticalImageScroller extends StatefulWidget {
+  final List<AssetEntity> _assets;
+
+  VerticalImageScroller(this._assets, {Key? key}) : super(key: key);
+  @override
+  _VerticalImageScrollerState createState() => _VerticalImageScrollerState();
+}
+
+class _VerticalImageScrollerState extends State<VerticalImageScroller> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemBuilder: _assetItemBuilder,
+        scrollDirection: Axis.horizontal,
+        itemCount: widget._assets.length,
+      ),
+      height: 100.0,
+    );
+  }
+
+  Widget _assetItemBuilder(BuildContext _, int index) {
+    return Container(
+      height: 60.0,
+      width: 60.0,
+      margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(5.0),
+          child: Stack(
+            children: <Widget>[
+              _imageWidget(index),
+              _deleteButton(index),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _removeAsset(int index) {
+    setState(() {
+      widget._assets.remove(widget._assets.elementAt(index));
+    });
+  }
+
+  Widget _imageWidget(int index) {
+    return Positioned.fill(
+      child: ExtendedImage(
+        image: AssetEntityImageProvider(
+          widget._assets.elementAt(index),
+          isOriginal: false,
+        ),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _deleteButton(int index) {
+    return Positioned(
+      top: 6.0,
+      right: 6.0,
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => _removeAsset(index),
+        child: Container(
+          padding: const EdgeInsets.all(2.0),
+          decoration: BoxDecoration(
+            color: Theme.of(context).dividerColor.withOpacity(0.5),
+            shape: BoxShape.circle,
+          ),
+          child: Icon(Icons.close, size: 14.0, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+}
