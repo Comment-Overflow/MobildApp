5599a3bd030008a23f501af5ada6b5902e5242c6
JolyneFr <jolynefr@sjtu.edu.cn>
2021-07-17 16:07:28 +0800

    tweak: add single image constructor for gallery dto

--- lib/model/routing_dto/image_gallery_dto.dart
+++ lib/model/routing_dto/image_gallery_dto.dart
@@ -6,4 +6,6 @@ class ImageGalleryDto {
   int get index => _index;
 
   ImageGalleryDto(this._imageUrl, this._index);
+  ImageGalleryDto.fromSingleImage(String url, this._index)
+      : this._imageUrl = <String>[url];
 }
