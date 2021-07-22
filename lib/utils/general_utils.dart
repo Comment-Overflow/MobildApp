ccd6c9cb17e646277799d974a56f4021046a7090
xx01cyx <caoyuanxin0531@outlook.com>
2021-07-22 14:40:58 +0800

    feat: add utils for getting token and userId from storage, add exception for unauthorization

--- lib/utils/general_utils.dart
+++ lib/utils/general_utils.dart
@@ -1,3 +1,6 @@
+import 'package:comment_overflow/assets/constants.dart';
+import 'package:comment_overflow/exceptions/user_unauthorized_exception.dart';
+import 'package:comment_overflow/utils/storage_util.dart';
 import 'package:dart_date/dart_date.dart';
 
 class GeneralUtils {
@@ -37,5 +40,19 @@ class GeneralUtils {
       return time.format('MM-dd HH:mm');
     else
       return time.format('yyyy-MM-dd HH:mm');
+  }
+
+  /// Get the current JWT token.
+  static Future<String> getCurrentToken() async {
+    String? token = await StorageUtil().storage.read(key: Constants.token);
+    if (token == null) throw UserUnauthorizedException();
+    return token;
+  }
+
+  /// Get the current user ID.
+  static Future<int> getCurrentUserId() async {
+    String? userId = await StorageUtil().storage.read(key: Constants.userId);
+    if (userId == null) throw UserUnauthorizedException();
+    return userId as int;
   }
 }
